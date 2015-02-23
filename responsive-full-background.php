ff0a68445f9b2bd0f8df80a31d1a15261b0cf650
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-23 14:18:06 +0000

    readme responsive-full-background
    git-svn-id: https://plugins.svn.wordpress.org/responsive-full-background/trunk@1097215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- responsive-full-background.php
+++ responsive-full-background.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Responsive Full Background
-Plugin URI: http://qass.im/my-plugins/
+Plugin URI: http://wp-time.com/responsive-full-background/
 Description: Add responsive full background to your website easily, compatible with all browsers and with iPhone, iPad, and all phone and tablets.
 Version: 1.0.0
 Author: Qassim Hassan
@@ -9,7 +9,7 @@ Author URI: http://qass.im
 License: GPLv2 or later
 */
 
-/*  Copyright 2014  Qassim Hassan  (email : qassim.pay@gmail.com)
+/*  Copyright 2015  Qassim Hassan  (email : qassim.pay@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
